e274b51d9c9c540efb4526f41e4a005e03204161
362572158 <362572158@2260I-L29-019>
2026-09-24 13:10:48 -0400

    hello

--- Monogame 5 - Baddie Class/Ghost.cs
+++ Monogame 5 - Baddie Class/Ghost.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +9,10 @@ using System.Threading.Tasks;
 namespace Monogame_5___Baddie_Class
 {
     public class Ghost
-{
-
-}
+    {
+        private List<Texture2D> _textures;
+        private Vector2 _speed;
+        private Rectangle _location;
+        private int _textureIndex;
+    }
 }
