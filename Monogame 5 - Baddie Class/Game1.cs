0873eb9f2cf19d7423ed6bfd733a930f0b6428be
362572158 <362572158@2260I-L29-019>
2026-09-25 13:20:21 -0400

    hello

--- Monogame 5 - Baddie Class/Game1.cs
+++ Monogame 5 - Baddie Class/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
 
 namespace Monogame_5___Baddie_Class
 {
@@ -8,7 +10,18 @@ namespace Monogame_5___Baddie_Class
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
+        List<Texture2D> ghostTextures;
+        List<Ghost> ghosts;
+        Texture2D titleScreen, houseScreen, endScreen, mario;
+        Random generator = new Random();
+        MouseState mouseState = new MouseState();
+        KeyboardState keyboardState = new KeyboardState();
+        enum Screen
+        {
+            Title, House, End
+        }
+        Screen screen;
+        Rectangle window;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -19,13 +32,35 @@ namespace Monogame_5___Baddie_Class
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            IsMouseVisible = false;
+            ghostTextures = new List<Texture2D>();
+            ghosts = new List<Ghost>();
+            window = new Rectangle(0, 0, 800, 600);
+            screen = Screen.Title;
             base.Initialize();
+            mouseState = Mouse.GetState();
+            for (int i = 0; i <= 20; i++)
+            {
+                Ghost temp = new Ghost(ghostTextures, new Rectangle(generator.Next(window.Width - 40), generator.Next(window.Height - 40), 40, 40));
+                while (temp.Contains(mouseState.Position))
+                    temp = new Ghost(ghostTextures, new Rectangle(generator.Next(window.Width - 40), generator.Next(window.Height - 40), 40, 40));
+
+                ghosts.Add(temp);
+            }
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            ghostTextures.Add(Content.Load<Texture2D>("boo-stopped"));
+            for (int i = 1;  i <= 8; i++)
+            {
+                ghostTextures.Add(Content.Load<Texture2D>("boo-move-" + i));
+            }
+            titleScreen = Content.Load<Texture2D>("haunted-title");
+            houseScreen = Content.Load<Texture2D>("haunted-background");
+            endScreen = Content.Load<Texture2D>("haunted-end-screen");
+            mario = Content.Load<Texture2D>("mario");
 
             // TODO: use this.Content to load your game content here
         }
@@ -36,7 +71,23 @@ namespace Monogame_5___Baddie_Class
                 Exit();
 
             // TODO: Add your update logic here
+            mouseState = Mouse.GetState();
+            keyboardState = Keyboard.GetState();
+            if (screen == Screen.Title)
+            {
+                if (keyboardState.IsKeyDown(Keys.Enter))
+                    screen = Screen.House;
 
+            }
+            else if (screen == Screen.House)
+            {
+                foreach (Ghost ghost in ghosts)
+                {
+                    ghost.Update(gameTime, mouseState);
+                    if (ghost.Contains(mouseState.Position))
+                        screen = Screen.End;
+                }
+            }
             base.Update(gameTime);
         }
 
@@ -45,7 +96,20 @@ namespace Monogame_5___Baddie_Class
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
 
+            if (screen == Screen.Title)
+                _spriteBatch.Draw(titleScreen, window, Color.White);
+            else if (screen == Screen.House)
+            {
+                _spriteBatch.Draw(houseScreen, window, Color.White);
+                _spriteBatch.Draw(mario, mouseState.Position.ToVector2(), Color.White);
+                foreach (Ghost ghost in ghosts)
+                    ghost.Draw(_spriteBatch);
+            }
+            else
+                _spriteBatch.Draw(endScreen, window, Color.White);
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
--- Monogame 5 - Baddie Class/Ghost.cs
+++ Monogame 5 - Baddie Class/Ghost.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,8 +13,81 @@ namespace Monogame_5___Baddie_Class
     public class Ghost
     {
         private List<Texture2D> _textures;
+        private float _animationSpeed;
+        private float _seconds;
+        private float _alpha;
         private Vector2 _speed;
         private Rectangle _location;
         private int _textureIndex;
+        private SpriteEffects _direction;
+
+        public Rectangle Rect
+        {
+            get { return _location; }
+        }
+        public Ghost(List<Texture2D> textures, Rectangle location)
+        {
+            _textures = textures;
+            _speed = Vector2.Zero;
+            _location = location;
+            _textureIndex = 0;
+            _direction = SpriteEffects.None;
+            _seconds = 0;
+            _animationSpeed = 0.2f;
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_textures[_textureIndex], _location, null, Color.White, 0f, Vector2.Zero, _direction, 1);
+        }
+        public void Update(GameTime gameTime, MouseState mouseState)
+        {
+            if (mouseState.X < _location.X)
+            {
+                _direction = SpriteEffects.FlipHorizontally;
+                _speed.X = -1;
+            }
+            else if (mouseState.X > _location.X)
+            {
+                _direction = SpriteEffects.None;
+                _speed.X = 1;
+            }
+            if (mouseState.Y < _location.Y)
+            {
+                _speed.Y = -1;
+            }
+            else if (mouseState.Y > _location.Y)
+            {
+                _speed.Y = 1;
+            }
+            if (mouseState.LeftButton == ButtonState.Released)
+            {
+                _speed = Vector2.Zero;
+                _textureIndex = 0;
+                _seconds = 0f;
+                _alpha = 0.3f;
+            }
+            else if (_speed != Vector2.Zero)
+            {
+                _seconds += (float)gameTime.ElapsedGameTime.TotalSeconds;
+                if (_seconds > _animationSpeed)
+                {
+                    _seconds = 0;
+                    _textureIndex++;
+                    if (_textureIndex >= _textures.Count)
+                        _textureIndex = 1;
+                    _speed = Vector2.Zero;
+                    _alpha = 1f;
+                }
+            }
+            _location.Offset(_speed);
+        }
+        public bool Contains(Point player)
+        {
+            return _location.Contains(player);
+        }
+        public bool Intersects(Rectangle player)
+        {
+            return _location.Intersects(player);
+        }
     }
 }
